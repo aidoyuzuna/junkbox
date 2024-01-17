904b080c1efcfbb1e4612b082c9104f63948eac8
saro <netsv1643+gi@gmail.com>
2024-01-17 22:22:40 +0900

    OpenAIで音声入力：ひとまず完成

--- openai/text-recording.py
+++ openai/text-recording.py
@@ -9,25 +9,27 @@ openai.api_key = setting.API_KEY
 
 
 class Widget:
-    def __init__(self):
+    def __init__(self,page):
+        self.page = page
         self.text_field = ft.TextField(max_lines=10, bgcolor="#333333", label="ここに出力結果がでます", read_only=True,
                                        value="")
         self.button = ft.ElevatedButton(text="録音開始", on_click=self.btn_recording)
 
-    @staticmethod
-    def btn_recording(e):
+    def btn_recording(self, e):
         r = sr.Recognizer()
         with sr.Microphone(sample_rate=16_000) as source:
             audio = r.listen(source)
             audio_data = BytesIO(audio.get_wav_data())
             audio_data.name = "from_mic.wav"
             transcript = openai.Audio.transcribe("whisper-1", audio_data)
-            e.page.update()
+            self.text_field.value = transcript["text"]
+            self.page.update()
+
 
 def setapp(page: ft.Page):
-    widgets = Widget()
+    widgets = Widget(page)
     page.title = "録音アプリ"
-    page.add(widgets.text_field,widgets.button)
+    page.add(widgets.text_field, widgets.button)
 
 
 def main():
