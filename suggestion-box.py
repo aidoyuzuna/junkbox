b7ffbdaa7df19747b6633dc9df06e7af779b975f
aidoyuzuna <hoshipaso+gi@gmail.com>
2024-03-23 13:24:48 +0900

    👍 目安箱課題：たたき台実装

--- suggestion-box.py
+++ suggestion-box.py
@@ -7,21 +7,39 @@ class Suggestion:
 # 目安箱
 class SuggestionBox:
     def __init__(self):
-        ...
+        self.box = []
 
     def __len__(self) -> int:
-
-    # 入っている数を返す
+        return len(self.box)
 
     def post(self, suggestion: Suggestion):
-
-    # 要望を箱に入れる
+        self.box.append(suggestion)
 
     def take_out(self) -> Suggestion:
-# 入っている要望をひとつ取り出す(なければエラー or None(どちらかで実装))
+        return self.box.pop(0)
+
 
 def main():
-    
+    box = SuggestionBox()
+    print(len(box))
+    sug1 = Suggestion("アップルパイをメニューに入れて")
+    box.post(sug1)
+
+    sug2 = Suggestion("ゴミ箱からゴミがあふれてる")
+    box.post(sug2)
+    sug1_out = box.take_out()
+    print(sug1_out.body)  # Call me
+
+    sug3 = Suggestion("体育館にあるネットがボロボロだから直して")
+    box.post(sug3)
+    print(len(box))  # 2
+
+    sug2_out = box.take_out()
+    print(sug2_out.body)  # Buy it
+    sug3_out = box.take_out()
+    print(sug3_out.body)  # Marry me
+    print(len(box))  # 0
+
 
 if __name__ == '__main__':
     main()
