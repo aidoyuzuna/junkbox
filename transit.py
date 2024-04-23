62f5ba470de926430305d35787938b2f0dac5c89
aidoyuzuna <hoshipaso+gi@gmail.com>
2024-04-23 21:39:53 +0900

    ✨ transit.pyにコメントを追加

--- transit.py
+++ transit.py
@@ -2,6 +2,7 @@ import swisseph as swe
 import datetime
 
 
+# 惑星クラスの作成
 class Planet:
     def __init__(self):
         self._sign = (
@@ -21,7 +22,8 @@ class Planet:
     def planet_return(self):
         return self._planet
 
-    def planet_calc(self, now, timezone: int):
+    # 惑星の位置を計算
+    def planet_calc(self, now: datetime, timezone: int):
         # 日付をUTC・ユリウス暦に変換
         utc = swe.utc_time_zone(now.year, now.month, now.day, now.hour, now.minute, now.second, timezone)
         jul = swe.utc_to_jd(*utc)
@@ -30,14 +32,15 @@ class Planet:
         calc = swe.calc(jul[0], self._planet_number)
         return calc
 
-    def decision_sign(self, planet):
-        # 星座判定（360度の円を12等分している。1つの星座につき30度）
+    # 惑星星座判定（360度の円を12等分している。1つの星座につき30度）
+    def decision_sign(self, planet: float):
         idx = int(planet // 30)
         if idx < 0 or idx > 11:
             raise ValueError(f"惑星{planet}は360以下の整数である必要がある: {planet}")  # エラーで原因が分かるようにする
         return self._sign[idx]
 
 
+# 各種惑星クラスの作成（planet_numberはsweライブラリー指定の数値）
 class Sun(Planet):
     def __init__(self):
         super().__init__()
@@ -108,6 +111,7 @@ class Pluto(Planet):
         self._planet_number = 9
 
 
+# 逆行判定（逆行がTrue）
 def return_planet(today, yesterday):
     if today < yesterday:
         return True
@@ -122,30 +126,44 @@ def main():
     yesterday_datetime = now_datetime - datetime.timedelta(days=1)
     timezone_offset = 9
 
+    # 惑星を作成
     today_planet = [Sun(), Moon(), Mercury(), Venus(), Mars(), Jupiter(), Saturn(), Uranus(), Neptune(), Pluto()]
     yesterday_planet = [Sun(), Moon(), Mercury(), Venus(), Mars(), Jupiter(), Saturn(), Uranus(), Neptune(), Pluto()]
+
+    # 取得する惑星名を入れる変数を設定
     planet_name = []
+
+    # 各種リストの初期化
     today_transit = []
     yesterday_transit = []
     today_sign = []
     return_flag = []
     result_text = f"【{now_datetime:%Y/%m/%d %H:%M} 現在のトランジット状況】\n"
 
+    # 惑星の計算・判定
     for i in range(10):
+        # 今日と昨日の惑星を計算
         today_transit.append(today_planet[i].planet_calc(now_datetime, timezone_offset))
         yesterday_transit.append(
             yesterday_planet[i].planet_calc(yesterday_datetime, timezone_offset))
 
+        # 惑星名の取得
         planet_name = today_planet[i].planet_return()
+
+        # 星座名の取得
         today_sign.append(today_planet[i].decision_sign(today_transit[i][0][0]))
+
+        # 逆行判定
         return_flag.append(return_planet(today_transit[i][0][0], yesterday_transit[i][0][0]))
 
+        # テキストの追加（逆行があるか否かで文章が変わる）
         if return_flag[i]:
             result_text += f"{planet_name}：{today_sign[i]}{int(today_transit[i][0][0] % 30)}度（逆行中）\n"
 
         else:
             result_text += f"{planet_name}：{today_sign[i]}{int(today_transit[i][0][0] % 30)}度\n"
 
+    # 結果を出力
     print(result_text)
 
 
