f0bed49787a4eccad90486ae43d5b0a5d244b7cb
aidoyuzuna <hoshipaso+gi@gmail.com>
2024-07-27 19:08:20 +0900

    ✨ get_image_listの引数名を変更、その他微修正

--- image-reduction-webp.py
+++ image-reduction-webp.py
@@ -1,23 +1,21 @@
 from pathlib import Path
-import sys
 from PIL import Image
 
 
-def get_image_list(input_list: Path) -> list[Path]:
+def get_image_list(input_path: Path) -> list[Path]:
     """画像のファイル一覧を取得する
 
-    :param input_list: Downloadsフォルダー
+    :param input_path: 画像の入力元フォルダ
     :return: .jpg,.pngファイル一覧のPathリスト
     """
 
     # 画像ファイル（jpg・png）検索
     extension = ['.jpg', '.png']
-    image_list = [i for i in Path(input_list).glob('**/*.*') if i.suffix in extension]
+    image_list = [i for i in input_path.glob('**/*.*') if i.suffix in extension]
 
     # jpg・png画像がない場合のエラー処理
     if not image_list:
-        print("エラー！Downloadsフォルダーにjpg・pngファイルがありません")
-        sys.exit(1)
+        raise FileNotFoundError("エラー！Downloadsフォルダーにjpg・pngファイルがありません")
 
     return image_list
 
@@ -26,14 +24,14 @@ def convert_image(convert_list: list[Path], save_path: Path):
     """画像を50％に縮小・webpに変換
 
     :param convert_list:get_image_listで取得した変換画像リスト
-    :param save_path: Desktopフォルダー
+    :param save_path: 画像の出力先フォルダ
     :return: none
     """
 
     for i in convert_list:
         save_file_path = save_path.joinpath(Path(i.stem))
         image = Image.open(i).convert('RGB')
-        resize_file = image.resize((image.width * 0.4, image.height * 0.4))
+        resize_file = image.resize((image.width // 3, image.height // 3))
         resize_file.save(save_file_path.with_suffix('.webp'), "webp", quality=80, method=6)
 
 
